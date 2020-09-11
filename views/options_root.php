cbd8e63d13e3f4f4b2a895e3bcfb2b4da34d01ba
Darren Miller <darren.miller@ihasco.co.uk>
2020-09-11 06:09:45 +0100

    Don't do that anymore

--- views/options_root.php
+++ views/options_root.php
@@ -3,11 +3,5 @@ CONGRALUTATIONS
 You've found the instructions for submitting a job application.
 
 To start, you need to POST a request to this endpoint.
-Include data containing your `email` and the `reference`
-of the position you wish to apply for.
-
-The resulting response will direct you to the `Location` of the next
-request and provide a `token` which should be supplied on all subsequent
-requests as a "Authorization: Bearer token" header.
-
-An OPTIONS request to the location provided will provide further instructions.
+Include data containing your `email` and the `reference` of the position you
+wish to apply for.
